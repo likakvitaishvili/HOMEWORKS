747aa1db4bbc8118b85edeb9389ce5df88fd3096
likakvitaishvili <51286363+likakvitaishvili@users.noreply.github.com>
2019-11-30 15:46:07 +0400

    Create GenerateInter.java

---
+++ homework4/generator/GenerateInter.java
@@ -0,0 +1,6 @@
+public interface GenerateInter {
+    public void generateAlphabet();
+    public char generateChar();
+    public String generateWord(int min, int max);
+    public String generateeSentenc(int min, int max);
+}
